0c1d3a05382620d51e8f4cb63f4014bff4a0a3fb
haidyeed <haidyeed@gmail.com>
2024-03-29 18:28:12 +0200

    user authentication

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,15 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'api' => [
+            'driver' => 'daily',
+            'path' => storage_path('logs/api/api.log'),
+            'level' => 'debug',
+            'permission' => 0775,
+            'days' => 2,
+
+        ],
     ],
 
 ];
